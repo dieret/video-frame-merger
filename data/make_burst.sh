90e5ff6718da2d0d21e507ba165ac8584e4ac3c3
klieret <klieret@users.noreply.github.com>
2018-06-18 22:50:46 +0200

    fix: don't only get incremental changes when bursting gif

--- data/make_burst.sh
+++ data/make_burst.sh
@@ -2,6 +2,8 @@
 
 mkdir -p burst
 for file in *.gif; do
-	convert "${file}" "${file%.gif}.png"
+    # echo $file
+	# note: coalesce: get full frames, not incremental differenes 
+	convert "${file}" -coalesce "${file%.gif}.png"
 	mv ${file%.gif}*.png burst/
 done
