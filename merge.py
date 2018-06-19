ec5e57b14a9e2e6d704e13d0e62b83559de8405f
Alvaro <alvaro@Alvaros-MBP.home>
2018-06-19 02:16:56 +0200

    automate burst and merge of gifs in ./data/giflibrary
    Added methods for detecting gifs in ./data/giflibrary,
    bursting them into newly created subfolders of ./data/burst
    and merging them into an identically named png in ./out

--- merge.py
+++ merge.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import cv2
-import os.path
+import os
 import glob
+import shutil
 from util import log
 import numpy as np
-
+from subprocess import call
 
 class Merger(object):
     def __init__(self):
@@ -103,6 +104,63 @@ class Merger(object):
     def get_merged_image(self):
         return self.merged_images / self.sum_weights.reshape(tuple(list(self.default_shape)[:-1]+[1]))
 
+def burst_destination_folder():
+    return os.path.join("data", "burst")
+
+def video_source_folder():
+    return os.path.join("data", "giflibrary")
+
+def burst_gifs():
+    get_clean_folder(burst_destination_folder())
+    files = get_video_files(video_source_folder())
+    for file in files:
+        burst_file(file)
+
+def file_extension(file):
+    return file.split(".")[-1]
+
+def strip_extension(file):
+    return ".".join(file.split(".")[0:-1])
+
+def get_video_files(folder):
+    files = os.listdir(folder)
+    return [file for file in files if file_extension(file) in {"gif"}]
+
+# clear folder if existent, create if nonexistent    
+def get_clean_folder(folder):
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+    os.mkdir(folder)
+
+# burst file into a clean subfolder of burst_folder
+def burst_file(file):
+    subfolder = os.path.join(burst_destination_folder(), strip_extension(file))
+    get_clean_folder(subfolder)
+    destination = os.path.join(subfolder, file + "%5d.png")
+    call(["ffmpeg", "-i", os.path.join(video_source_folder(), file), destination]) 
+
+
+def burst_and_merge_gifs():
+    burst_gifs()
+    for file in get_video_files(video_source_folder()):
+        merge_gif(file)
+
+# copy of baxter()
+def merge_gif(file):
+    image_name = strip_extension(file)
+    burst_folder = os.path.join(burst_destination_folder(), image_name)
+    
+    # note: unsorted, but we don't care about that right now.
+    paths = glob.glob(os.path.join(burst_folder, "*.png"))
+    m = Merger()
+    for path in paths:
+        m.add_to_mean(path)
+    m.save_image(m.mean_image, "out/mean.png")
+    for path in paths:
+        m.add_to_merged(path)
+    m.save_image(m.merged_images, "out/" + image_name + "_merged.png")
+    m.save_image(m.get_merged_image(), "out/" + image_name + "_merged-normed.png")
+    
 
 def baxter():
     base_path = os.path.join("data", "burst")
@@ -119,4 +177,4 @@ def baxter():
 
 if __name__ == "__main__":
     # m = Merger()
-    baxter()
+    burst_and_merge_gifs()
