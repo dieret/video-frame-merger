3b97fc0638db2be8e19130277898f30bb1211df3
klieret <klieret@users.noreply.github.com>
2018-06-19 09:25:33 +0200

    update burst scripts; .gitignore

--- data/make_burst_ffmpeg.sh
+++ data/make_burst_ffmpeg.sh
@@ -1,1 +1,10 @@
-ffmpeg -i baxter.gif burst/baxter-%03d.png
+#!/bin/bash
+
+mkdir -p burst
+for file in giflibrary/*.gif; do
+    # echo $file
+	# note: coalesce: get full frames, not incremental differenes 
+	ffmpeg -i "${file}" ${file%.gif}-%03d.png
+	mv ${file%.gif}*.png burst/
+done
+
